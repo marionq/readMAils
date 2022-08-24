ee6705769d92e8165e194b208811979a2c790dbd
Nepomuceno <monu@gft.com>
2022-08-24 02:14:12 -0500

    Se agregó archivo de volumenes

--- read.py
+++ read.py
@@ -39,19 +39,34 @@ for message in messages:
 
         if dateBackup.month == dateOut.month and dateBackup.day == dateOut.day:
             idsVol.append(dicBody["data"]["additionalDetails"]["volumeId"])
-            jsonVol.append(
-                {
-                    'volumeId': dicBody["data"]["additionalDetails"]["volumeId"],
-                    'compartmentName': dicBody["data"]["compartmentName"],
-                    'eventTime': dateBackup.strftime("%Y-%m-%d %H:%M:%S"),
-                    'backupState': dicBody["data"]["additionalDetails"]["backupState"]
-                }
-            )
+
+            if '.begin' in dicBody["eventType"]:
+                jsonVol.append(
+                    {
+                        'volumeId': dicBody["data"]["additionalDetails"]["volumeId"],
+                        'volumeName': '',
+                        'compartmentName': dicBody["data"]["compartmentName"],
+                        'eventTime': dateBackup.strftime("%Y-%m-%d %H:%M:%S"),
+                        'backupState': dicBody["data"]["additionalDetails"]["backupState"]
+                    }
+                )
+            else:
+                jsonVol.append(
+                    {
+                        'volumeId': dicBody["data"]["additionalDetails"]["volumeId"],
+                        'volumeName': dicBody["data"]["additionalDetails"]["volumeName"],
+                        'compartmentName': dicBody["data"]["compartmentName"],
+                        'eventTime': dateBackup.strftime("%Y-%m-%d %H:%M:%S"),
+                        'backupState': dicBody["data"]["additionalDetails"]["backupState"]
+                    }
+                )
 
 if cont <= 0:
     print("No hay mensajes sobre OCI con la fecha requerida en la bandeja de entrada, por favor seleccione algun otro directorio")
     quit()
 
+# Read volumens.xlsx
+dfV = pd.read_excel('volumes.xlsx')
 # Conversion de list Dictionary a dataframe
 df = pd.DataFrame(jsonVol, dtype="datetime64[ns]")
 # Borrado de registro duplicados
@@ -64,16 +79,17 @@ dfA = dfR.query("backupState=='AVAILABLE'")
 dfRR = pd.merge(dfP, dfA, how='outer', left_on='volumeId', right_on='volumeId')
 # Remplazar los valores NaN por "X"
 dfRR = dfRR.replace(np.nan, '')
-# Case
+#
 dfRR['compartmentName_y'] = np.where(dfRR['compartmentName_y'] == '', dfRR['compartmentName_x'], dfRR['compartmentName_y'])
 dfRR['compartmentName_x'] = np.where(dfRR['compartmentName_x'] == '', dfRR['compartmentName_y'], dfRR['compartmentName_x'])
-dfRR = dfRR[['volumeId', 'compartmentName_x', 'eventTime_x', 'backupState_x', 'eventTime_y', 'backupState_y']]
-dfRR = dfRR.rename(columns={'compartmentName_x':'compartmentName', 'eventTime_x':'eventTime_inicio', 'backupState_x':'backupState_inicio', 'eventTime_y':'eventTime_fin', 'backupState_y':'backupState_fin'})
+dfRR = dfRR[['volumeId', 'volumeName_y', 'compartmentName_x', 'eventTime_x', 'backupState_x', 'eventTime_y', 'backupState_y']]
+dfRR = dfRR.rename(columns={'volumeName_y':'volumeName', 'compartmentName_x':'compartmentName', 'eventTime_x':'eventTime_inicio', 'backupState_x':'backupState_inicio', 'eventTime_y':'eventTime_fin', 'backupState_y':'backupState_fin'})
 
+dfRS = pd.merge(dfRR, dfV, how='outer', left_on='volumeId', right_on='volumeId')
 
 # Creación de archivo de Excel
 writer = pd.ExcelWriter(f'infVolumeBackup_{dateOut.day}-{dateOut.month}-{dateOut.year}.xlsx')
 # Escritura de dataframe en archivo de excel
-dfRR.to_excel(writer)
+dfRS.to_excel(writer)
 # Guardado de información en archivo de excel
 writer.save()
